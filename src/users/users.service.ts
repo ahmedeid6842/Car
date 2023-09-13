c314ecf37e04ea06174206c6d122c92487874e44
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-13 05:31:48 +0300

    Implement create&find for user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
+
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.userRepo.create({ email, password });
+
+    return this.userRepo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.userRepo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.userRepo.find({ where: { email } });
+  }
 }
