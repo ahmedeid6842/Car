83e23d8ec0dec0e580fd993c98af570f0dabbfbf
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-13 05:32:14 +0300

    Implement update and remove user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,4 +20,23 @@ export class UsersService {
   find(email: string) {
     return this.userRepo.find({ where: { email } });
   }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.userRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.userRepo.remove(user);
+  }
 }
