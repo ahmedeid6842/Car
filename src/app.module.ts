2d931a52113c6fcc2fe481679bc2efbbef2430e5
Ahmed Eid <ahmedeid6842@gmail.com>
2023-09-13 04:57:40 +0300

    Revert "Persist data  Using Typeorm"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ReportsModule } from './reports/reports.module';
-import { UsersModule } from './users/users.module';
-import { Report } from './reports/reports.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [Report],
-      synchronize: true,
-    }),
-    ReportsModule,
-    UsersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
