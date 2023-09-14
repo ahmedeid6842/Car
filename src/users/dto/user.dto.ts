69b3bff4f2e44b4e1467308df83548533419ba8f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-14 06:31:53 +0300

    Added the user dto for outgoing responses

---
+++ src/users/dto/user.dto.ts
@@ -0,0 +1,11 @@
+import { Expose } from 'class-transformer';
+
+export class UserDto {
+  // specify this property will be exposed to responses
+  @Expose()
+  id: number;
+
+  // sepcify this peropty will be exposed to responses
+  @Expose()
+  email: string;
+}
