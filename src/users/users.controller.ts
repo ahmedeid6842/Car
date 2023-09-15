3221c1a30ad25d3139e70daa039c3c3b80318377
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-15 10:16:57 +0300

    Implement Auth.signin controller method to use AuthService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,11 +20,19 @@ import { AuthService } from './auth.service';
 @Serialize(UserDto)
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService, private authSerivce: AuthService) { }
+  constructor(private userService: UsersService, private authService: AuthService) { }
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto, @Session() session: any) {
-    const user = await this.authSerivce.signup(body.email, body.password);
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
     
     return user;
