a87ff95286cfa822c9ed1417da1c26b3b2c56497
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-15 09:52:09 +0300

    Implement Auth.signup service method

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
-
+import bcrypt from "bcrypt"
 @Injectable()
 export class AuthService {
     constructor(private userService: UsersService) { }
+
+    async signup(email: string, password: string) {
+        const users = await this.userService.find(email)
+
+        if (users.length) {
+            throw new BadRequestException(`this email:${email} is already exists`)
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        password = await bcrypt.hash(password, salt);
+
+        const user = await this.userService.create(email, password);
+
+        return user;
+    }
 }
