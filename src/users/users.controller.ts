dfcd57a744e9934ab8edc110843a827cf8b46b61
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-19 03:42:57 +0300

    Implement Auth.signout controller method to use AuthService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -34,8 +34,13 @@ export class UsersController {
   async signin(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
-    
+
     return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
   }
 
   @Get('/:id')
