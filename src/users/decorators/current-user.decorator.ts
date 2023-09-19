7ac5040e012ab42015dce8f23f522e245fe1a07e
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-19 03:53:06 +0300

    Setup the basic Getuser Decorators

---
+++ src/users/decorators/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+export const CurrentUser = createParamDecorator(
+    //  data of type never to specify it won't receive anthing
+    //  context of type ExuctionContenct to be suitable for any type of connection http or websocket ... etc
+    (data: never, context: ExecutionContext) => {
+        const request = context.switchToHttp().getRequest(); // convert the context of incoming request to http to  can extract the session id
+        return request.session.userId;
+    }
+)
