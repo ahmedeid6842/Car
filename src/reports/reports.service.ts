3d4d49bdd1e9bf435ba501a0e13da49985d3e1e4
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 05:50:41 +0300

    Added changeApproval service with addition of approved to entity

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { Repository } from 'typeorm';
@@ -12,6 +12,17 @@ export class ReportsService {
     create(reportDto: CreateReportDto, user: User) {
         const report = this.reports.create(reportDto)
         report.user = user;
+        return this.reports.save(report);
+    }
+
+    async changeApproval(id: string, approved: boolean) {
+        const report = await this.reports.findOne({ where: { id: parseInt(id) } });
+
+        if (!report) {
+            throw new NotFoundException("report not found")
+        }
+
+        report.approved = approved
         return this.reports.save(report);
     }
 }
