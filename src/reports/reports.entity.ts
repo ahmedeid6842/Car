4c7a0d97ee5750052cba8e36181ddfd26ab768d9
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 03:42:11 +0300

    Added a new properties to reports entities

--- src/reports/reports.entity.ts
+++ src/reports/reports.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column() 
+  model: string;
+
+  @Column()
+  year: string;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }
