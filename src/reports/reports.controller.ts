e78a2e1c36cd86a4488358167cb65e708d5181a2
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-20 03:57:54 +0300

    Implememnt create report controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/users.entity';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportService: ReportsService) { }
+    @Post()
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+        return this.reportService.create(body)
+    }
+}
